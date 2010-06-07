120680b985c3e9d1469578503b7639d48d911e19
micholi <micholi@7946bc56-f69c-b45b-1acb-643ca4523ef9>
2010-06-07 04:59:01 +0000

    added integral reduced gematria method

--- src/gematriacalculator/GematriaCalculator.java
+++ src/gematriacalculator/GematriaCalculator.java
@@ -4,15 +4,17 @@ public class GematriaCalculator {
 	public GematriaMethod[] methods;
 
 	public GematriaCalculator() {
-		methods = new GematriaMethod[9];
+		methods = new GematriaMethod[11];
 		methods[0] = new GematriaMethodAbsolute();
 		methods[1] = new GematriaMethodAbsoluteSofit();
 		methods[2] = new GematriaMethodOrdinal();
 		methods[3] = new GematriaMethodOrdinalSofit();
 		methods[4] = new GematriaMethodReduced();
 		methods[5] = new GematriaMethodReducedSofit();
-		methods[6] = new GematriaMethodAtbash();
-		methods[7] = new GematriaMethodAlbam();
-		methods[8] = new GematriaMethodAvgad();
+		methods[6] = new GematriaMethodIntegralReduced();
+		methods[7] = new GematriaMethodIntegralReducedSofit();
+		methods[8] = new GematriaMethodAtbash();
+		methods[9] = new GematriaMethodAlbam();
+		methods[10] = new GematriaMethodAvgad();
 	}
 }
